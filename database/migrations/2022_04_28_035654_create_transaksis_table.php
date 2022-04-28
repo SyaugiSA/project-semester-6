cabe1ca159181222256113f318415d0b2d09f428
Syaugi SA <syaugiamar9@gmail.com>
2022-04-28 11:47:01 +0700

    menambah dan update model dan migration

---
+++ database/migrations/2022_04_28_035654_create_transaksis_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('donasi_id')->references('id')->on('donasis')->onDelete('cascade');
+            $table->integer('jumlah');
+            $table->string('bukti');
+            $table->tinyInteger('is_verified')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksis');
+    }
+}
--- database/migrations/2022_04_28_035705_create_comments_table.php
+++ database/migrations/2022_04_28_035705_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateJadwalKhatibsTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateJadwalKhatibsTable extends Migration
      */
     public function up()
     {
-        Schema::create('jadwal_khatibs', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->date('tanggal');
-            $table->foreignId('khatib_id')->constrained('khatibs')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('artikel_id')->references('id')->on('artikels')->onDelete('cascade');
+            $table->string('body');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -28,6 +30,6 @@ class CreateJadwalKhatibsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('jadwal_khatibs');
+        Schema::dropIfExists('comments');
     }
 }
