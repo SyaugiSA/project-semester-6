f2797c8da76dc57b19c07c6ae767d8ab109d0b23
priska19 <priska360@gmail.com>
2022-04-30 22:50:27 +0700

    route event

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventUser\EventController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('User.partial.home');
 });
+
+Route::get('/event',[EventController::class,'index']);
