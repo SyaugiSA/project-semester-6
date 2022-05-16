a0faa7432f969a85fbc6a1cf4abaac64f8e9bedd
Syaugi SA <syaugiamar9@gmail.com>
2022-05-16 11:50:01 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArtikelController;
+use App\Http\Controllers\DonateController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -26,21 +28,26 @@ Route::get('/about', function(){
     return view('User.About Us.about');
 });
 
-
-Route::get('/donate', function () {
-    return view('User.halaman.donate');
+Route::prefix('/doate')->group(function(){
+    Route::get('/', [DonateController::class, 'index']);
+    Route::get('/{id}', [DonateController::class, 'show']);
+    Route::get('/create', [DonateController::class, 'create']);
+    Route::post('/create', [DonateController::class, 'store']);
+    Route::get('/{id}/edit', [DonateController::class, 'edit']);
+    Route::put('/{id}/edit', [DonateController::class, 'update']);
+    Route::delete('/{id}', [DonateController::class, 'destroy']);
 });
 
-Route::get('/donate/detail', function () {
-    return view('User.halaman.donation-detail');
+Route::prefix('/artikel')->group(function(){
+    Route::get('/', [ArtikelController::class, 'index']);
+    Route::get('/{id}', [ArtikelController::class, 'show']);
+    Route::get('/create', [ArtikelController::class, 'create']);
+    Route::post('/create', [ArtikelController::class, 'store']);
+    Route::get('/{id}/edit', [ArtikelController::class, 'edit']);
+    Route::put('/{id}/edit', [ArtikelController::class, 'update']);
+    Route::delete('/{id}', [ArtikelController::class, 'destroy']);
 });
 
-Route::get('/artikel', function () {
-    return view('User.halaman.artikel');
-});
-Route::get('/artikel/detail', function () {
-    return view('User.halaman.artikel-detail');
-});
 Route::get('/seting', function () {
     return view('User.halaman.seting-akun');
 });
